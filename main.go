e2f2c546e7490771877c6f51d948b04d1c4b3c6a
Kristian Mide <fas@mide.dk>
2020-06-24 20:18:38 +0200

    cleaned up main function

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"log"
-	"net"
 
 	"github.com/fasmide/remotemoe/http"
 	"github.com/fasmide/remotemoe/router"
@@ -22,37 +21,17 @@ func main() {
 	server := http.NewServer()
 	server.Handler = proxy
 
-	for _, port := range services.Services["HTTP"] {
-		l, err := net.ListenTCP("tcp", &net.TCPAddr{Port: port})
-		if err != nil {
-			// listen errors are usually not a big deal, maybe the user just
-			// dont have permissions on privileged-ports but we are still able to work
-			// on higher portnumbers
-			log.Printf("cannot accept HTTP on %d: %s", port, err)
-			continue
-		}
-
-		log.Printf("accepting %s on %d", "HTTP", port)
-		go server.Serve(l)
-	}
+	services.Serve("HTTP", server)
 
 	sshConfig, err := ssh.DefaultConfig()
 	if err != nil {
 		log.Fatalf("cannot get default ssh config: %s", err)
 	}
 
-	sshServer := ssh.Server{Config: sshConfig, Router: router}
+	sshServer := &ssh.Server{Config: sshConfig, Router: router}
 
-	for _, port := range services.Services["SSH"] {
-		l, err := net.ListenTCP("tcp", &net.TCPAddr{Port: port})
-		if err != nil {
-			log.Printf("cannot accept SSH on %d: %s", port, err)
-			continue
-		}
+	services.Serve("SSH", sshServer)
 
-		log.Printf("accepting %s on %d", "SSH", port)
-		go sshServer.Serve(l)
-	}
-
+	// we shall be dealing with shutting down in the future :)
 	select {}
 }
