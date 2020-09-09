2a2a68ca8e55ed9aaa20145caafc36fe8a43e7da
Kristian Mide <fas@mide.dk>
2020-09-09 21:05:56 +0200

    command: dont default to list hostnames, show the user the possibilities instead

--- ssh/command/host.go
+++ ssh/command/host.go
@@ -10,35 +10,32 @@ import (
 
 // Host returns a *cobra.Command that enables the user to mange custom hosts
 func Host(r router.Routable) *cobra.Command {
-	list := func(cmd *cobra.Command, _ []string) error {
-		namedRoutes, err := router.Names(r)
-		if err != nil {
-			return fmt.Errorf("unable to lookup your custom names: %w", err)
-		}
-
-		if len(namedRoutes) == 0 {
-			cmd.Printf("No active hostnames.\n")
-			return nil
-		}
-
-		cmd.Printf("Active hostnames:\n")
-		for _, nr := range namedRoutes {
-			cmd.Printf("%s\n", nr.FQDN())
-		}
-
-		return nil
-	}
-
 	top := &cobra.Command{
 		Use:   "host",
 		Short: "Manage hostnames",
-		RunE:  list,
 	}
 
 	top.AddCommand(&cobra.Command{
 		Use:   "list",
 		Short: "List hostnames",
-		RunE:  list,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			namedRoutes, err := router.Names(r)
+			if err != nil {
+				return fmt.Errorf("unable to lookup your custom names: %w", err)
+			}
+
+			if len(namedRoutes) == 0 {
+				cmd.Printf("No active hostnames.\n")
+				return nil
+			}
+
+			cmd.Printf("Active hostnames:\n")
+			for _, nr := range namedRoutes {
+				cmd.Printf("%s\n", nr.FQDN())
+			}
+
+			return nil
+		},
 	})
 
 	top.AddCommand(host.Remove(r))
