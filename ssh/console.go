d9d7a2faa27f7ef49e30ba038fb5aa67701f12d3
Kristian Mide <fas@mide.dk>
2020-09-09 21:59:53 +0200

    console: autocompletion

--- ssh/console.go
+++ ssh/console.go
@@ -63,6 +63,78 @@ func (c *Console) Accept(channelRequest ssh.NewChannel) error {
 		}
 	}()
 
+	main := DefaultCmd(c.session)
+	main.SetOut(term)
+
+	term.AutoCompleteCallback = func(line string, pos int, key rune) (newLine string, newPos int, ok bool) {
+		// If we don't receive TAB, simply return without doing anything.
+		if key != '\t' {
+			return line, pos, false
+		}
+
+		prefix := line[:pos]
+		postfix := line[pos:]
+
+		line = line[:pos] + string(key) + line[pos:]
+
+		wordBeforeCursor := prefix
+
+		spacePos := strings.LastIndex(prefix, " ")
+		if spacePos >= 0 {
+			wordBeforeCursor = prefix[spacePos+1:]
+		}
+
+		var suggestions []string
+
+		command := main
+		args := strings.Fields(line)
+
+		if found, _, err := command.Find(args); err == nil {
+			command = found
+		}
+
+		// If the cursor is placed at the end of a matched command, add a space. Like bash.
+		if command.Name() == wordBeforeCursor {
+			return prefix + " " + postfix, pos + 1, true
+		}
+
+		if command.HasAvailableSubCommands() {
+			for _, c := range command.Commands() {
+				name := c.Name()
+
+				if !c.Hidden && strings.HasPrefix(name, wordBeforeCursor) {
+					suggestions = append(suggestions, name)
+				}
+			}
+		}
+
+		// If we have exactly one match, simply use it.
+		if len(suggestions) == 1 {
+			// If the cursor is at the end, insert a space too like bash.
+			if postfix == "" {
+				postfix = " "
+				pos++
+			}
+
+			return prefix + suggestions[0][len(wordBeforeCursor):] + postfix, pos + len(suggestions[0]) - len(wordBeforeCursor), true
+		}
+
+		if len(suggestions) > 1 {
+			whiteSpace := ""
+			for i := 0; i < len(prefix)+2; i++ {
+				whiteSpace += " "
+			}
+
+			// Trick term into keeping the current line.
+			fmt.Fprintf(term, "$ %s\033[33m...\033[0m%s\n\r", prefix, postfix)
+			for _, s := range suggestions {
+				fmt.Fprintf(term, "\033[33m%s%s\033[0m\n", whiteSpace, s)
+			}
+		}
+
+		return prefix + postfix, pos, false
+	}
+
 	go func() {
 		defer channel.Close()
 		for {
@@ -72,13 +144,16 @@ func (c *Console) Accept(channelRequest ssh.NewChannel) error {
 					return
 				}
 
-				if strings.TrimSpace(cmd) != "" {
-					main := DefaultCmd(c.session)
-					main.SetOut(term)
-
-					main.SetArgs(strings.Fields(cmd))
-					_ = main.Execute()
+				if strings.TrimSpace(cmd) == "" {
+					continue
 				}
+
+				main.SetArgs(strings.Fields(cmd))
+				_ = main.Execute()
+
+				// the main commands needs its flags reverted to their default value
+				// for every invocation
+				CommandReset(main)
 
 			case msg, ok := <-c.session.msgs:
 				if !ok {
