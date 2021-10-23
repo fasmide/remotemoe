e8f7099da3b6ad07b36e29701fd17ec9b2618473
Kristian Mide <fas@mide.dk>
2021-10-23 23:42:43 +0200

    buildvars: added comments

--- buildvars/buildvars.go
+++ buildvars/buildvars.go
@@ -4,8 +4,17 @@ package buildvars
 // buildvars have been initialized and should be displayed.
 var Initialized string
 
+// GitCommit hash of current commit
 var GitCommit string
+
+// GitCommitDate the date and time this commit was made
 var GitCommitDate string
+
+// GitBranch the current branch
 var GitBranch string
+
+// GitRepository the repository where this build was made
 var GitRepository string
+
+// GitPorcelain describes files which was not committed
 var GitPorcelain string
